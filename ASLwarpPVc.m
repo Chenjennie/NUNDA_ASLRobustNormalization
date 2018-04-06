a82fe780c26422a61cb8c4421a97e5313dd97544
Chenjennie <33103404+Chenjennie@users.noreply.github.com>
2018-04-06 11:25:50 -0500

    Update ASLwarpPVc.m

--- ASLwarpPVc.m
+++ ASLwarpPVc.m
@@ -65,7 +65,8 @@ try
         matlabbatch{1,end}.spm.tools.vbm8.estwrite.data{1,1}=head;
         matlabbatch{1}.spm.tools.vbm8.estwrite.extopts.dartelwarp.normhigh.darteltpm   = {which('Template_1_IXI550_MNI152.nii')};
         matlabbatch{1}.spm.tools.vbm8.estwrite.opts.tpm = {which('TPM.nii')};
-        
+        matlabbatch{1}.spm.tools.vbm8.estwrite.extopts.dartelwarp.normhigh.darteltpm
+        matlabbatch{1}.spm.tools.vbm8.estwrite.opts.tpm
         try
             spm_jobman('initcfg');
             spm_jobman('run_nogui',matlabbatch);
